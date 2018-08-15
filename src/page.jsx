f77dc6a0e953f031ee75754cba7ecf2b56e7ab18
Muhammad Muhsin <muhammad.muhseen@gmail.com>
2018-08-15 10:21:00 +0530

    Fixed: 404 on page.jsx

--- src/page.jsx
+++ src/page.jsx
@@ -15,7 +15,7 @@ class Post extends React.Component {
         var url = window.location.href.split('/');
         var slug = url.pop() || url.pop();
 
-        fetch(CelestialSettings.URL.api + "/pages?slug=" + slug)
+        fetch(CelestialSettings.URL.api + "pages?slug=" + slug)
             .then(function (response) {
                 if (!response.ok) {
                     throw Error(response.statusText);
